d08121a2d7539e435d0a028611d71fd33b0f8cbd
Michał Adamczyk <michal.anks@gmail.com>
2018-03-22 18:54:31 +0100

    Add provisional validation interface.

--- src/Validation.hs
+++ src/Validation.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE DataKinds #-}
@@ -10,7 +11,7 @@ import Serialization
 import Data.Binary
 
 
-data ValidationError = InvalidContract Text
+data ValidationError = MarkedAsInValidError
   deriving(Eq,Show)
 
 newtype Untrusted a = Untrusted a
@@ -22,3 +23,21 @@ newtype Untrusted a = Untrusted a
 
 untrust :: a -> Untrusted a
 untrust = Untrusted
+
+
+------------- provisional validator implementation -----------------
+
+newtype Validator e m a = MkValidator { validFun :: Untrusted a -> m (Either e a) }
+
+
+makeValidator :: Functor m => (Untrusted a -> m Bool) -> Validator ValidationError m a
+makeValidator boolFun = MkValidator validationFun
+  where validationFun x@(Untrusted trusted) = flip fmap (boolFun x) $ \case
+          False -> Left MarkedAsInValidError
+          True  -> Right trusted
+
+makeValidatorWithCustomError :: Functor m => (Untrusted a -> m (Either e a)) -> Validator e m a
+makeValidatorWithCustomError = MkValidator
+
+validate :: Validator e m a -> Untrusted a -> m (Either e a)
+validate (MkValidator vfun) = vfun
