62f2cceab347ce3b48062105ebd6c6c178f2cff9
Michał Adamczyk <michal.anks@gmail.com>
2018-03-27 05:08:55 +0200

    Finish AsyncChain building functions. Add first ad-hoc tests.

--- src/AsyncChain.hs
+++ src/AsyncChain.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 
 
@@ -20,6 +22,11 @@ import System.Random
 data AsyncChain :: [*] -> * where
   AsyncChainDone :: AsyncChain '[]
   AsyncChainStep :: Async (a, IO (AsyncChain as)) -> AsyncChain (a:as)
+
+infixr 1 >:$
+(>:$) :: (a -> IO b) -> HandlerChain as bs -> HandlerChain (a : as) (b : bs)
+(>:$) = HandleStep
+
 
 data HandlerChain :: [*] -> [*] -> * where
   HFin :: HandlerChain '[] '[]
@@ -62,24 +69,18 @@ pollAll (AsyncChainStep t) = do
         (pfl, err) <- pollAll rest
         return (PrefixCons x pfl, err)
 
+----------
 
-
-type family ListRev (as :: [*]) :: [*] where
-  ListRev xs = ListRevHelper xs '[]
-
-type family ListRevHelper (as :: [*]) (bs :: [*]) :: [*] where
-  ListRevHelper '[] bs = bs
-  ListRevHelper (x ': xs) bs = ListRevHelper xs (x ': bs)
-
-
-
-data HndlH a b c = HndlS (IO (b,c))
-                 | HndlA (a->IO(b,c))
 
 
 data AsyncChainBuilder :: [*] -> [*] -> *  where
-  AsyncChainBuilderStart :: IO (a, b) -> AsyncChainBuilder (a ': '[]) (b ': '[])
-  AsyncChainBuilderAppend :: (b0 -> IO (a,b)) -> AsyncChainBuilder as (b0 ': bs) -> AsyncChainBuilder (a ': as) (b ': b0 ': bs)
+  AsyncChainStart :: IO (a, b) -> AsyncChainBuilder (a : '[]) (b : '[])
+  AsyncChainAppend :: (b0 -> IO (a,b)) -> AsyncChainBuilder as (b0 : bs) -> AsyncChainBuilder (a : as) (b ': b0 ': bs)
+
+infixl 1 &:>
+(&:>) :: AsyncChainBuilder as (b0 : bs) -> (b0 -> IO (a,b)) -> AsyncChainBuilder (a : as) (b ': b0 ': bs)
+(&:>) = flip AsyncChainAppend
+
 
 
 
@@ -87,83 +88,78 @@ data AsyncChainHelper :: [*] -> [*] -> * where
   AsyncChainHelperDone :: AsyncChainHelper '[] '[]
   AsyncChainHelperStep :: Async (a, b, b -> IO (AsyncChainHelper as bs)) -> AsyncChainHelper (a:as) (b:bs)
 
+unHelperTemp :: AsyncChainHelper as bs -> AsyncChain as
+unHelperTemp AsyncChainHelperDone = AsyncChainDone
+unHelperTemp (AsyncChainHelperStep helperAsync) = AsyncChainStep $ (\(a, b, getNextAsync) -> (a, unHelperTemp <$> getNextAsync b)) <$> helperAsync
 
 
--- buildAsyncChain :: AsyncChainBuilder (a ': as) (b ': bs) -> IO (AsyncChainHelper (ListRev (a ': as)))
--- buildAsyncChain (AsyncChainBuilderAppend action bld) = case bld of
---     (AsyncChainBuilderAppend _ _) -> undefined
-
--- buildAsyncChain :: AsyncChainBuilder a b -> IO (AsyncChainHelper (ListRev a))
--- buildAsyncChain (AsyncChainBuilderStart action) = async action >>= \s -> return $ AsyncChainHelperStep $ (\(ss,rs) -> (ss,rs,return . const AsyncChainHelperDone)) <$> s
--- buildAsyncChain aschainbld@(AsyncChainBuilderAppend action bld) = undefined--go aschainbld
-
---   where
---     go (AsyncChainBuilderAppend action bld) = do
-
---       previousSteps <- go bld
-
---       undefined
-  
+-------- building async chain -----------------
 
 
+start2StepAsyncChain :: AsyncChainBuilder '[ r1, r0 ] '[ a1, a0] -> IO (AsyncChain '[ r0, r1])
+start2StepAsyncChain bld = startStep (finStep bld)
+
+
+start3StepAsyncChain :: AsyncChainBuilder '[ r2, r1, r0 ] '[ a2, a1, a0] -> IO (AsyncChain '[ r0, r1, r2])
+start3StepAsyncChain bld = startStep (step (finStep bld))
+
+
+start4StepAsyncChain :: AsyncChainBuilder '[ r3, r2, r1, r0 ] '[ a3, a2, a1, a0] -> IO (AsyncChain '[ r0, r1, r2, r3])
+start4StepAsyncChain bld = startStep (step (step (finStep bld)))
+
+start5StepAsyncChain bld = startStep (step (step (step (finStep bld))))
+
+start6StepAsyncChain bld = startStep (step (step (step (step (finStep bld)))))
+
+start7StepAsyncChain bld = startStep (step (step (step (step (step (finStep bld))))))
+
+start8StepAsyncChain bld = startStep (step (step (step (step (step (step (finStep bld)))))))
+
+
+step (currBldr, nextStep) = (nextSteps currBldr, \x-> AsyncChainHelperStep .
+        fmap (\(a,b) -> (a,b, nextStep)) <$> async (asyncChainStepAction currBldr x))
+
+
+finStep bld = step (bld, const $ return AsyncChainHelperDone)
+
+
+startStep (currBldr, nextStep) = unHelperTemp . AsyncChainHelperStep .
+  fmap (\(a,b) -> (a,b, nextStep)) <$> async (asyncChainStartAction currBldr)
+
+
+nextSteps :: AsyncChainBuilder (x : xs) (y : y1 : ys) -> AsyncChainBuilder xs (y1:ys)
+nextSteps (AsyncChainAppend _ bld) = bld
+
+
+asyncChainStepAction  :: AsyncChainBuilder (x : xs) (y : y1 : ys) -> (y1 -> IO (x,y))
+asyncChainStepAction  (AsyncChainAppend action _) = action
+
+
+asyncChainStartAction :: AsyncChainBuilder '[x] '[y] -> IO (x,y)
+asyncChainStartAction (AsyncChainStart action) = action
+
+---------- not necessary for now
+
+-- type family ListRev (as :: [*]) :: [*] where
+--   ListRev xs = ListRevHelper xs '[]
+
+-- type family ListRevHelper (as :: [*]) (bs :: [*]) :: [*] where
+--   ListRevHelper '[] bs = bs
+--   ListRevHelper (x ': xs) bs = ListRevHelper xs (x ': bs)
 
 
 
-
-  -- collectActions (HCons action HNil) bld >>= undefined
-  -- where
-
--- collectActions :: HList cs -> AsyncChainBuilder (a ': as) (b ': b0 ': bs) -> (HList (HndlH b0 a b  ': cs))
--- collectActions acc (AsyncChainBuilderAppend action bld) = let newacc = HCons (HndlA action) acc in case bld of
---   (AsyncChainBuilderAppend action' bld') -> collectActions newacc bld
+-------------------------------------------------------------------
+--------------               testing             ------------------
+-------------------------------------------------------------------
 
 
-collectStartAction :: HList cs -> AsyncChainBuilder (a ': '[] ) (b ': '[] ) -> (HList (HndlH b0 a b  ': cs))
-collectStartAction acc (AsyncChainBuilderStart action) = HCons (HndlS action) acc
-
--- buildAsyncChain (AsyncChainBuilderAppend action bld) = do
-
---   let lastStep x = AsyncChainHelperStep <$> do
---         asy <- async $ action x
---         return $ (\(a,b) -> (a,b, const $ return AsyncChainHelperDone)) <$> asy
-
---   (AsyncChainHelperStep prevStepsAsyncChain) <- buildAsyncChain bld
-
-
---   return $ AsyncChainHelperStep $ (\(x,y) -> (x,y, lastStep)) <$> prevStepsAsyncChain
---   --   nextAsyncAction = async $ do
-
---   undefined
-
--------------- testing
 infixr 0 &
 (&) :: a -> (a->b) -> b
 arg & f = f arg
 
 testAsyncChainBldr :: AsyncChainBuilder '[ Bool, Int ] '[ Bool, Int]
-testAsyncChainBldr = AsyncChainBuilderStart (randomIO >>= \x -> return (x,x)) & AsyncChainBuilderAppend (\x -> return (even x, even x))
-
-
--------- building async chain -----------------
-
-tempConverter :: AsyncChainBuilder '[ a, b ] '[ c, d ] -> IO (AsyncChainHelper '[ b, a ] '[ d, c ])
-tempConverter (AsyncChainBuilderAppend action bld) = do
-
-  let lastStep x = AsyncChainHelperStep <$> do
-        asy <- async $ action x
-        return $ (\(a,b) -> (a,b, const $ return AsyncChainHelperDone)) <$> asy
-
-  mysteryFun bld lastStep
-
-
-mysteryFun :: AsyncChainBuilder '[ b ] '[ d ] -> ( d -> IO (AsyncChainHelper '[a] '[c])) -> IO (AsyncChainHelper '[ b, a] '[ d, c])
-mysteryFun (AsyncChainBuilderStart action) nextStep = AsyncChainHelperStep <$> do
-  asy <- async action
-  return $ (\(a,b) -> (a,b, nextStep)) <$> asy
-
-
-
-----------------------------------------
+testAsyncChainBldr = AsyncChainStart (randomIO >>= \x -> return (x,x)) & AsyncChainAppend (\x -> return (even x, even x))
 
 
 testAsyncChainHelper :: IO (AsyncChainHelper '[ Int, Bool] '[Int, Bool])
@@ -178,11 +174,6 @@ testAsyncChainHelper = do
 
 
 
-unHelperTemp :: AsyncChainHelper as bs -> AsyncChain as
-unHelperTemp AsyncChainHelperDone = AsyncChainDone
-unHelperTemp (AsyncChainHelperStep helperAsync) = AsyncChainStep $ (\(a, b, getNextAsync) -> (a, unHelperTemp <$> getNextAsync b)) <$> helperAsync
-
-
 testAsyncChain :: IO (AsyncChain '[ Int, Bool])
 testAsyncChain = unHelperTemp <$> testAsyncChainHelper
 
@@ -195,20 +186,29 @@ waitHandleSteps (AsyncChainStep asyncedChain) (HandleStep hndle handlers) = do
   nextStep <- startNext
   HCons r <$> waitHandleSteps nextStep handlers
 
-infixr 1 >:$
-(>:$) :: (a -> IO b) -> HandlerChain as bs -> HandlerChain (a : as) (b : bs)
-(>:$) = HandleStep
-
 
 testStepsAsyncChain :: IO ()
 testStepsAsyncChain = testAsyncChain >>= \aschain -> void $ waitHandleSteps aschain (print >:$ print >:$ HFin)
+
+test1 :: IO Bool
+test1 = do
+  aschain <- start3StepAsyncChain $ AsyncChainStart ((randomIO :: IO Int) >>= \x->return (x, x)) &:>
+                                                     (\x -> heavyComputation >> return (even x, even x)) &:>
+                                                     (\x -> heavyComputation >> return (not x, x))
+
+  hLast <$> waitHandleSteps aschain (handleStepResult >:$ handleStepResult >:$ handleStepResult >:$ HFin)
+
+
+  where handleStepResult x = putStr "handling: " >> print x >> return x
+        heavyComputation = threadDelay 1500000
+
 
 
 
 
 -- buildAsyncChain :: AsyncChainBuilder a b -> IO (AsyncChainHelper (ListRev a))
--- buildAsyncChain (AsyncChainBuilderStart action) = async action >>= \s -> return $ AsyncChainHelperStep $ (\(ss,rs) -> (ss,rs,const AsyncChainHelperDone)) <$> s
--- buildAsyncChain (AsyncChainBuilderAppend action bld) = do
+-- buildAsyncChain (AsyncChainStart action) = async action >>= \s -> return $ AsyncChainHelperStep $ (\(ss,rs) -> (ss,rs,const AsyncChainHelperDone)) <$> s
+-- buildAsyncChain (AsyncChainAppend action bld) = do
 --   prevStepsAsyncChain <- buildAsyncChain bld
 --   nextAsyncAction = async $ do
 
